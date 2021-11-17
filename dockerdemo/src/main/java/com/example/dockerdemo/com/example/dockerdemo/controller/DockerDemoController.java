68bb111da542b8f3dbe718bcc74651ce5c8d5949
Mohammad Mustaque Alam <mustaque26@gmail.com>
2021-11-17 18:42:15 +0530

    Created a sample Restful project

---
+++ dockerdemo/src/main/java/com/example/dockerdemo/com/example/dockerdemo/controller/DockerDemoController.java
@@ -0,0 +1,17 @@
+package com.example.dockerdemo.com.example.dockerdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class DockerDemoController {
+
+    @RequestMapping("/api1")
+    @GetMapping
+    public String homepage(){
+        return "home API 1";
+    }
+
+}
